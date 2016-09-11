327279f55ba31cc37007ad43ca65fe26d64480e4
Alex Andriishyn <alex.andriishyn@gmail.com>
2016-09-11 12:38:35 -0400

    Refactored UserController to use the new UserDB class

--- src/java/cms/controllers/UserController.java
+++ src/java/cms/controllers/UserController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cms.controllers;
 
 import java.io.IOException;
@@ -10,16 +5,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import cms.business.User;
-import cms.data.ConnectionPool;
 import java.sql.*;
 import java.util.*;
 
-/**
- *
- * @author TankJr
- */
+import cms.business.User;
+import cms.data.*;
+
 public class UserController extends HttpServlet {
     
     @Override
@@ -34,33 +25,7 @@ public class UserController extends HttpServlet {
         if (action.equals("add")) {
             url = "/add.jsp";
         } else if (action.equals("list")) {
-            ConnectionPool pool = ConnectionPool.getInstance();
-            Connection conn = pool.getConnection();
-            Statement stmt = null;
-            ResultSet rs = null;
-            
-            try {
-                String sql = "SELECT * FROM Users";
-                stmt = conn.createStatement();
-                rs = stmt.executeQuery(sql);
-                
-                ArrayList<User> users = new ArrayList<>();
-                while (rs.next()) {
-                    User u = new User();
-                    u.setId(rs.getInt("id"));
-                    u.setFirstName(rs.getString("firstName"));
-                    users.add(u);
-                }
-                request.setAttribute("users", users);
-                
-                rs.close();
-                stmt.close();
-            } catch (SQLException e) {
-                System.err.println(e);
-            }
-            
-            pool.freeConnection(conn);
-                
+            request.setAttribute("users", UserDB.list());    
             url = "/list.jsp";
         }
         
@@ -72,21 +37,9 @@ public class UserController extends HttpServlet {
             throws ServletException, IOException {
         
         String firstName = request.getParameter("firstName");
-        User user = new User(firstName);
-           
-        ConnectionPool pool = ConnectionPool.getInstance();
-        Connection conn = pool.getConnection();
-        PreparedStatement ps = null;
         
-        try {
-            String preparedSQL = "INSERT INTO Users(firstName) VALUES ( ? )";
-            ps = conn.prepareStatement(preparedSQL);
-            ps.setString(1, user.getFirstName());
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            for (Throwable t : e)
-                t.printStackTrace();
-        }
+        User newUser = new User(firstName);
+        UserDB.insert(newUser);
         
         // Show updated list of all users
         response.sendRedirect("users?action=list");
--- src/java/cms/data/UserDB.java
+++ src/java/cms/data/UserDB.java
@@ -1,5 +1,81 @@
 package cms.data;
 
+import cms.business.User;
+import cms.data.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+// Handles DB ops
 public class UserDB {
-    
+
+    public static void insert(User newUser) {
+
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection conn = pool.getConnection();
+        PreparedStatement ps = null;
+
+        try {
+            String preparedSQL = "INSERT INTO Users(firstName) VALUES ( ? )";
+            ps = conn.prepareStatement(preparedSQL);
+            ps.setString(1, newUser.getFirstName());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            for (Throwable t : e) {
+                t.printStackTrace();
+            }
+        }
+
+        DBUtil.closePreparedStatement(ps);
+        pool.freeConnection(conn);
+    }
+
+    public static void delete(User deletedUser) {
+        // Delete routine
+    }
+
+    public static void update(User updatedUser) {
+        // Update routine
+    }
+
+    public static User getUser(int id) {
+        // Get a specified user routine
+
+        return new User();
+    }
+
+    public static ArrayList<User> list() {
+        // List all users routine
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection conn = pool.getConnection();
+        Statement stmt = null;
+        ResultSet rs = null;
+        ArrayList<User> users = null;
+        
+        try {
+            String sql = "SELECT * FROM Users";
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
+            users = new ArrayList<>();
+            
+            while (rs.next()) {
+                User u = new User();
+                u.setId(rs.getInt("id"));
+                u.setFirstName(rs.getString("firstName"));
+                users.add(u);
+            }
+
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
+
+        DBUtil.closeResultSet(rs);
+        DBUtil.closeStatement(stmt);
+        pool.freeConnection(conn);
+        
+        return users;
+    }
 }
